f63305d1e33dcbfcaeaa0bd83aa56bdb0c60bed0
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-06-06 11:18:31 +0100

    Prepare configuartion

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,24 @@
 use criterion::Criterion;
 use pyo3::prelude::*;
+use std::path::PathBuf;
 
 #[pyfunction(name = "benchmark")]
 fn pyterion_decorator(function: &PyAny) -> PyResult<()> {
-    let mut benchmark = Criterion::default();
-    benchmark.bench_function("Pyterion Benchmark", |bench| {
+    /* The following will become parameters for the decorator */
+    let benchmark_name = "Pyterion Benchmark";
+    let output_directory = "pyterion";
+    let significance_level = 0.05;
+    let confidence_level = 0.95;
+
+    let mut benchmark = Criterion::default()
+        .output_directory(&PathBuf::from(output_directory))
+        .confidence_level(confidence_level)
+        .significance_level(significance_level);
+
+    benchmark.bench_function(benchmark_name, |bench| {
         bench.iter(|| function.call0().expect("Error"))
     });
+
     Ok(())
 }
 
