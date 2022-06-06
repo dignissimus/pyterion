ca89722812d3765bd67c17b26344110ad5fe693d
Sam Ezeh <sam.z.ezeh@gmail.com>
2022-06-06 07:13:46 +0100

    Remove boilerplate

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 use criterion::Criterion;
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
-
 #[pyfunction(name = "benchmark")]
 fn pyterion_decorator(function: &PyAny) -> PyResult<()> {
     let mut benchmark = Criterion::default();
